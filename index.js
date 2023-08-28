fd662f83666900f03d03a34f88187a90409f6ffc
Ayeshafatimach2 <ayeshafatimach5@gmail.com>
2023-08-27 21:44:59 -0700

    Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+let number = 9;
+let multiplier = 1;
+while (multiplier <= 10) {
+    let result = number * multiplier;
+    console.log(number + " x " + multiplier + " = " + result);
+}
+//
+// let number = 19;
+// let multi = 1;
+// while (multi <= 1) {
+//     let result: number = number * multi;
+//     console.log(number + "x" + multi + "=" + result);
+// }
